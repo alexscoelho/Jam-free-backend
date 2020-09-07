45ba77049982d76fd7aefde204b3a32025bda981
alexscoelho <alexsoncoelho21@hotmail.com>
2020-09-07 21:00:02 +0000

    user,teacher,studen models #21

--- src/models.py
+++ src/models.py
@@ -4,9 +4,24 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80), unique=False, nullable=False)
+    last_name = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    account_type = db.Column(db.String(80), unique=False, nullable=False)
+    language = db.Column(db.String(80), unique=False, nullable=False)
+    teacher = db.relationship('Teacher', backref='user', uselist=False lazy=True)
+    student = db.relationship('Student', backref='user', uselist=False lazy=True)
+
+class Teacher(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primery_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
 
     def __repr__(self):
         return '<User %r>' % self.username
