27cebaced34d2a0bfe55f43bc4fc5db18b511808
alexscoelho <alexsoncoelho21@hotmail.com>
2020-09-10 02:49:11 +0000

    user, teacher, student models

--- src/models.py
+++ src/models.py
@@ -10,21 +10,28 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=False, nullable=False)
     account_type = db.Column(db.String(80), unique=False, nullable=False)
     language = db.Column(db.String(80), unique=False, nullable=False)
-    teacher = db.relationship('Teacher', backref='user', uselist=False lazy=True)
-    student = db.relationship('Student', backref='user', uselist=False lazy=True)
+    teacher = db.relationship('Teacher', backref='user', uselist=False, lazy=True)
+    student = db.relationship('Student', backref='user', uselist=False, lazy=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.account_type
 
 class Teacher(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    instrument= db.Column(db.String(120), unique=False, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Teacher %r>' % self.user_id
 
 class Student(db.Model):
-    id = db.Column(db.Integer, primery_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    instrument = db.Column(db.String(120), unique=False, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
 
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.user_id
 
     def serialize(self):
         return {
