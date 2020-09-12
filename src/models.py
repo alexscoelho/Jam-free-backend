033bbef8730a5f8898e3cc38d37706c409b8268b
johanuribe03 <johan.uribe@gmail.com>
2020-09-12 00:18:26 +0000

    Files-update1

--- src/models.py
+++ src/models.py
@@ -43,6 +43,32 @@ class Student(db.Model):
     def __repr__(self):
         return '<User %r>' % self.id
 
+class Files(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    instrument = db.Column(db.String(120), unique=False, nullable=False)
+    type_file = db.Column(db.String(120), unique=False, nullable=False)
+    level = db.Column(db.String(120), unique=False, nullable=False)
+    language = db.Column(db.String(120), unique=False, nullable=False)
+    teacher = db.relationship('Teacher', backref='user', uselist=False, lazy=True)
+
+    def __repr__(self):
+            return '<Files %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "instrument": self.instrument,
+            "last_name": self.last_name,
+            "email": self.email,
+            "account_type": self.account_type,
+            "language": self.language
+        }
+
+    
+
+
+
+
     
 
         
